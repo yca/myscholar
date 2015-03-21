6919b3e3372b29a44981b85fcafaf9991e1b1e03
Yusuf Caglar Akyuz <caglar@bilkon-kontrol.com.tr>
2015-03-21 09:46:30 +0200

    Add new features to browser.
    * Searching is implemented.
    * Manual tab addition is implemented.
    * Tab closing implemented.

--- mybrowser.cpp
+++ mybrowser.cpp
@@ -6,6 +6,7 @@
 #include <QWebView>
 #include <QCloseEvent>
 #include <QMessageBox>
+#include <QInputDialog>
 #include <QWebSettings>
 #include <QWebInspector>
 #include <QNetworkProxy>
@@ -15,17 +16,24 @@ MyBrowser::MyBrowser(QWidget *parent) :
 	ui(new Ui::MyBrowser)
 {
 	ui->setupUi(this);
+	ui->frameSearch->hide();
+	ui->tabWidget->setTabsClosable(true);
 }
 
 void MyBrowser::addTab(const QUrl &url)
 {
 	if (!urls.contains(url)) {
 		QWebView *view = new QWebView(ui->tabWidget);
+		MyWebPage *page = new MyWebPage(view);
+		connect(page, SIGNAL(newTabRequest(QUrl)), SLOT(addTab(QUrl)));
+		view->setPage(page);
+		connect(view, SIGNAL(linkClicked(QUrl)), SLOT(addTab(QUrl)));
+		view->installEventFilter(this);
 		view->page()->networkAccessManager()->setProxy(QNetworkProxy(QNetworkProxy::HttpProxy, "proxy.baskent.edu.tr", 8080, "hiler", "hil68er"));
 		view->load(url);
+		ui->listTabs->addItem(QString("%1").arg(url.toString()));
 		ui->tabWidget->addTab(view, QString("%1").arg(urls.size() + 1));
 		urls.insert(url, ui->tabWidget->count() - 1);
-		ui->listTabs->addItem(QString("%1").arg(url.toString()));
 	}
 	ui->tabWidget->setCurrentIndex(urls[url]);
 	ui->lineAddressBar->setText(url.toString());
@@ -34,6 +42,22 @@ void MyBrowser::addTab(const QUrl &url)
 MyBrowser::~MyBrowser()
 {
 	delete ui;
+}
+
+bool MyBrowser::eventFilter(QObject *obj, QEvent *ev)
+{
+	if (ev->type() != QEvent::KeyPress)
+		return false;
+	QKeyEvent *kev = (QKeyEvent *)ev;
+	if (kev->modifiers() != Qt::ControlModifier)
+		return false;
+	if (kev->key() != Qt::Key_F)
+		return false;
+	QWebView *view = qobject_cast<QWebView *>(obj);
+	if (!view)
+		return false;
+	initSearch(view);
+	return true;
 }
 
 void MyBrowser::closeEvent(QCloseEvent *ev)
@@ -49,5 +73,90 @@ void MyBrowser::on_listTabs_itemDoubleClicked(QListWidgetItem *item)
 
 void MyBrowser::on_tabWidget_currentChanged(int index)
 {
-	ui->lineAddressBar->setText(ui->listTabs->item(index - 1)->text());
+	if (index == 0) {
+		/* add page action */
+		QString text = QInputDialog::getText(this, trUtf8("URL Entry"), trUtf8("Please enter page address:"));
+		if (!text.isEmpty())
+			addTab(QUrl(text));
+	} else
+		ui->lineAddressBar->setText(ui->listTabs->item(index - 1)->text());
+}
+
+void MyBrowser::initSearch(QWebView *view)
+{
+	ui->frameSearch->show();
+	ui->lineSearch->setText(view->selectedText());
+	ui->lineSearch->setSelection(0, ui->lineSearch->text().size());
+	ui->lineSearch->setFocus();
+	on_lineSearch_textEdited(ui->lineSearch->text());
+}
+
+void MyBrowser::on_toolSearchClose_clicked()
+{
+	ui->frameSearch->hide();
+}
+
+void MyBrowser::on_lineSearch_textEdited(const QString &arg1)
+{
+	QWebView *view = (QWebView *)ui->tabWidget->currentWidget();
+	view->findText("", QWebPage::HighlightAllOccurrences); /* clear old selection */
+	if (!view->findText(arg1, QWebPage::HighlightAllOccurrences))
+		ui->lineSearch->setStyleSheet("color: red");
+	else
+		ui->lineSearch->setStyleSheet("");
+}
+
+void MyBrowser::on_pushSearchNext_clicked()
+{
+	QWebView *view = (QWebView *)ui->tabWidget->currentWidget();
+	view->findText(ui->lineSearch->text(), QWebPage::FindWrapsAroundDocument);
+}
+
+void MyBrowser::on_pushSearchPrev_clicked()
+{
+	QWebView *view = (QWebView *)ui->tabWidget->currentWidget();
+	view->findText(ui->lineSearch->text(), QWebPage::FindWrapsAroundDocument | QWebPage::FindBackward);
+}
+
+void MyBrowser::on_lineSearch_returnPressed()
+{
+	on_pushSearchNext_clicked();
+}
+
+void MyBrowser::on_tabWidget_tabCloseRequested(int index)
+{
+	if (!index)
+		return;
+	if (ui->tabWidget->count() < 3)
+		return;
+	QWidget *w = ui->tabWidget->widget(index);
+	ui->tabWidget->removeTab(index);
+	QListWidgetItem *item = ui->listTabs->takeItem(index - 1);
+	urls.remove(item->text());
+	ui->tabWidget->setCurrentIndex(1);
+	w->deleteLater();
+	delete item;
+}
+
+
+MyWebPage::MyWebPage(QObject *parent)
+	: QWebPage(parent)
+{
+	connect(this, SIGNAL(linkHovered(QString,QString,QString)), SLOT(linkHoveredS(QString,QString,QString)));
+}
+
+void MyWebPage::triggerAction(QWebPage::WebAction act, bool checked)
+{
+	if (act == QWebPage::OpenLinkInNewWindow)
+		emit newTabRequest(QUrl(lastLink));
+	else
+		QWebPage::triggerAction(act, checked);
+}
+
+void MyWebPage::linkHoveredS(const QString &link, const QString &title, const QString &textContent)
+{
+	Q_UNUSED(title);
+	Q_UNUSED(textContent);
+	if (!link.isEmpty())
+		lastLink = link;
 }
--- mybrowser.h
+++ mybrowser.h
@@ -4,12 +4,28 @@
 #include <QUrl>
 #include <QHash>
 #include <QWidget>
+#include <QWebPage>
 
+class QWebView;
 class QListWidgetItem;
 
 namespace Ui {
 class MyBrowser;
 }
+
+class MyWebPage : public QWebPage
+{
+	Q_OBJECT
+public:
+	MyWebPage(QObject *parent = NULL);
+	void triggerAction(WebAction act, bool checked = false);
+signals:
+	void newTabRequest(QUrl);
+protected slots:
+	void linkHoveredS(const QString &link, const QString &title, const QString &textContent);
+protected:
+	QString lastLink;
+};
 
 class MyBrowser : public QWidget
 {
@@ -17,16 +33,31 @@ class MyBrowser : public QWidget
 
 public:
 	explicit MyBrowser(QWidget *parent = 0);
-	void addTab(const QUrl &url);
 	~MyBrowser();
+public slots:
+	void addTab(const QUrl &url);
 protected:
+	bool eventFilter(QObject *obj, QEvent *ev);
 	void closeEvent(QCloseEvent *ev);
 private slots:
 	void on_listTabs_itemDoubleClicked(QListWidgetItem *item);
 
 	void on_tabWidget_currentChanged(int index);
 
+	void on_toolSearchClose_clicked();
+
+	void on_lineSearch_textEdited(const QString &arg1);
+
+	void on_pushSearchNext_clicked();
+
+	void on_pushSearchPrev_clicked();
+
+	void on_lineSearch_returnPressed();
+
+	void on_tabWidget_tabCloseRequested(int index);
+
 private:
+	void initSearch(QWebView *view);
 	Ui::MyBrowser *ui;
 	QHash<QUrl, int> urls;
 };
